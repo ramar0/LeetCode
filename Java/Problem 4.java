c3f25aafe266a9f6e13ba1411a780f23c75a77a4
ramar0 <roberto.amaro@encora.com>
2025-07-10 16:22:59 -0600

    Create Problem 4.java

---
+++ Java/Problem 4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (0 == p.length()){
+            return 0 == s.length();
+        }
+    
+        int i_s = 0;
+        int i_p = 0;
+        Boolean res = false;
+
+        while (i_s < s.length() && i_p < p.length()){
+        
+            res = (i_s < length(s) && (p.charAt(i_p) == s.charAt(i_s) or p.charAt(i_p) == '.'));
+
+            if (!res){
+                break;
+            }
+
+            if (i_p+1 < p.length() && p.charAt(i_p+1) == '*'){
+                if (i_s+1 < s.length() && s.charAt(i_s+1) != p.charAt(i_p)){
+                    i_p += 2;
+                }else{
+                    i_s++;
+                }
+            }else{
+                i_p++;
+                i_s++;
+            }
+        }
+        return res;
+    }
+}
