a269fe6e2863450da1edf9bc7e0e3a53b5674854
ramar0 <roberto.amaro@encora.com>
2025-07-13 22:57:57 -0600

    Create unequal-triplets.rs

---
+++ Individual Work/unequal-triplets.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn unequal_triplets(nums: Vec<i32>) -> i32 {
+        let countMap = HashMap::new();
+        let res = 0;
+
+        for i in nums{
+            count.entry(i).or_insert(0) += 1;
+        }
+
+        let count = countMap.values().cloned().collect();
+
+        let reps: Vec<_> = count.values().cloned().collect();
+
+        for k in 2..nums.len() {
+            for j in 1..k {
+                for i in ..j{
+                    res += reps[i]*reps[j]*reps[k];
+                }
+            }
+        }
+
+        res
+    }
+}
