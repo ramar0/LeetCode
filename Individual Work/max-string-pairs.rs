d32d566563dc04d315ba591fd8725b7d3460455c
ramar0 <roberto.amaro@encora.com>
2025-07-13 22:58:45 -0600

    Create max-string-pairs.rs

---
+++ Individual Work/max-string-pairs.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn maximum_number_of_string_pairs(words: Vec<String>) -> i32 {
+        let mut wordMap = HashMap::New();
+        let mut res = 0;
+
+        for word in words {
+            let rev = word.chars().rev().collect();
+
+            if let Some(exist) = map.get_mut(&rev) {
+                *exist-=1;
+                if *exist == 0 {
+                    map.remove(&exist);
+                }
+                res+=1;
+            }else{
+                *wordMap.entry(word.to_string()).or_insert(0)+=1;
+            }
+        }
+
+        res
+    }
+}
