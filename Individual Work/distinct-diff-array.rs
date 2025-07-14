83bc874a978715fc2aa2167756db0e3563917022
ramar0 <roberto.amaro@encora.com>
2025-07-13 23:18:47 -0600

    Update distinct-diff-array.rs
    Fixed typo in ln10

--- Individual Work/distinct-diff-array.rs
+++ Individual Work/distinct-diff-array.rs
@@ -1,13 +1,13 @@
 impl Solution {
     pub fn distinct_difference_array(nums: Vec<i32>) -> Vec<i32> {
-        let res = Vec::new();
+        let mut res = Vec::new();
 
         for i in 0..nums.len() {
             let prefix = nums[..=i];
             let suffix = nums[i+1..];
 
             let prefix_count = prefix.iter().collect::<HashSet>();
-            let suffic_count = suffix.iter().collect::<HashSet>();
+            let suffix_count = suffix.iter().collect::<HashSet>();
 
             res.push(prefix_count.len() - suffix_count.len());
         }
